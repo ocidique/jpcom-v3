dbda228b63b12b74d528dd67ad28bff40ac02152
Juho Pekkala <juho.pekkala@gmail.com>
2026-01-10 14:50:58 +0200

    chore: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,17 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://juhopekkala.com",
   integrations: [mdx(), sitemap()],
+
   vite: {
     // @ts-ignore
     plugins: [tailwindcss()],
   },
+
+  adapter: netlify(),
 });
