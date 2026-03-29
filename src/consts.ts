465dc0c38f539fd88bedd90f46c8b3ae9833a0eb
Juho Pekkala <juho.pekkala@gmail.com>
2026-03-29 18:22:52 +0300

    feat: add general style adjustments

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Juho Pekkala";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_HEADER_TITLE = "JuhoPekkala";
+export const SITE_DESCRIPTION = "Professional software developer and designer";
